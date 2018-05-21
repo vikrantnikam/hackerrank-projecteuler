8c225f7b4616d667166983b9c0798a8c5fd9799d
Vikrant Nikam <vikrant.nikam@dektech.com.au>
2018-05-21 19:37:14 +0200

    Solution - Project euler problem 1

---
+++ src/problem1/MultiplesOf3And5.java
@@ -0,0 +1,23 @@
+package problem1;
+
+import java.util.Scanner;
+
+public class MultiplesOf3And5 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int a0 = 0; a0 < t; a0++) {
+			int n = in.nextInt();
+			System.out.println(getMultipleOfN(n, 5) + getMultipleOfN(n, 3) - getMultipleOfN(n, (5 * 3)));
+
+		}
+		in.close();
+	}
+
+	private static long getMultipleOfN(int n, long val) {
+		long dividend = (n - 1) / val;
+		long sumofMultipleOfVal = val * dividend * (dividend + 1) / 2;
+		return sumofMultipleOfVal;
+	}
+}
